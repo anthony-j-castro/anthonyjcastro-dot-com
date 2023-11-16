9cb198f19410cd79397f6f5b1aac4eabb0791adf
Anthony Castro <anthony618@gmail.com>
2023-11-16 18:09:56 -0500

    Add initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("node:path");
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.(png)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+  },
+  plugins: [
+    new Dotenv(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"),
+          to: path.resolve(__dirname, "build"),
+          globOptions: {
+            ignore: path.resolve(__dirname, "public", "index.html"),
+          },
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    port: 3000,
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+  },
+};
