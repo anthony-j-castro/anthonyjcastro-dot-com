787b69fc9bf24f67aa5ee4cb9de331ed6c75da7d
Anthony Castro <anthony618@gmail.com>
2026-05-09 15:52:13 -0400

    Update playwright settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run build:e2e && npm run serve:e2e",
+    command: "npm run build:e2e && npm run preview:e2e",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     stdout: "ignore",
